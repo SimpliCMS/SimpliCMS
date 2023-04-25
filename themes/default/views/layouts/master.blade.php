5abd919c3f3390a5e14559fa9a287797f541ba38
Joshua Webb <joshwebb84@outlook.com>
2023-04-24 21:44:27 -0600

    Simplified code for the Menu blade template

--- themes/default/views/layouts/master.blade.php
+++ themes/default/views/layouts/master.blade.php
@@ -31,45 +31,18 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
-                            @include('partials.menu', ['menu' => Core::getMenu('Main Menu')])
+                            @include('partials.menu.main', ['menu' => Core::getMenu('Main Menu')])
                         </ul>
                         <!-- Right Side Of Navbar -->
-                        <!-- Authentication Links -->
                         @guest
-                        {!! showMenu('Guest menu', $alignment = 'right') !!}
+                        <ul class="navbar-nav ml-auto">
+                            @include('partials.menu.main', ['menu' => Core::getMenu('Guest Menu')])
+                        </ul>
                         @else
                         <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('cart.show') }}">Cart
-                                    @if (Cart::isNotEmpty())
-                                    <span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span>
-                                    @endif
-                                </a>
-                            </li>
-                            @role('admin')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
-                            </li>
-                            @endrole
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                            @endguest
+                           @include('partials.menu.main', ['menu' => Core::getMenu('User Menu')])
                         </ul>
+                        @endguest
                     </div>
                 </div>
             </nav>
---
+++ themes/default/views/partials/menu/main.blade.php
@@ -0,0 +1,36 @@
+@foreach($menu->items as $item)
+@if($item->parent_id === null)
+<li class="nav-item @if($item->children->count() > 0) dropdown @endif">
+    @if($item->name == 'Admin')
+    @role('admin')
+    <a class="nav-link @if($item->children->count() > 0) dropdown-toggle @endif" href="@if($item->is_internal == 1){{ route($item->url) }}@else{{ $item->url }}@endif" id="{{ $item->name }}" @if($item->children->count() > 0) role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @endif>{{ $item->name }}</a>
+    @endrole
+    @else
+    <a class="nav-link @if($item->children->count() > 0) dropdown-toggle @endif" href="@if($item->is_internal == 1){{ route($item->url) }}@else{{ $item->url }}@endif" id="{{ $item->name }}" @if($item->children->count() > 0) role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @endif @if($item->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($item->name == 'Account Menu'){{ Auth::user()->name }}@elseif($item->name == 'Cart'){{$item->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $item->name }}@endif</a>
+    @endif
+    @if($item->name == 'Logout')
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+    @endif    
+    @if($item->children->count() > 0)
+    <ul class="dropdown-menu" aria-labelledby="{{ $item->name }}">
+        @foreach($item->children as $childItem)
+        @if($item->name == 'Admin')
+        @role('admin')
+        <li><a class="dropdown-item" href="@if($childItem->is_internal == 1){{ route($childItem->url) }}@else{{ $childItem->url }}@endif">{{ $childItem->name }}</a></li>
+        @endrole
+        @else
+        <li><a class="dropdown-item" href="@if($childItem->is_internal == 1){{ route($childItem->url) }}@else{{ $childItem->url }}@endif" @if($childItem->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($childItem->name == 'Account Menu'){{ Auth::user()->name }}@elseif($childItem->name == 'Cart'){{$childItem->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $childItem->name }}@endif</a></li>
+        @endif
+        @if($childItem->name == 'Logout')
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+        @endif
+        @endforeach
+    </ul>
+    @endif
+</li>
+@endif
+@endforeach
