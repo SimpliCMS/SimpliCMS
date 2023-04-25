d78e0f2b07856e4fb282e094359da32df6237b35
Joshua Webb <joshwebb84@outlook.com>
2023-04-25 01:07:05 -0600

    Added a User Controller for the User Module front end as frontend pages for user manager directly from the user in progress and added user avatar to the user menu on front end

--- themes/default/views/partials/menu/main.blade.php
+++ themes/default/views/partials/menu/main.blade.php
@@ -6,7 +6,7 @@
     <a class="nav-link @if($item->children->count() > 0) dropdown-toggle @endif" href="@if($item->is_internal == 1){{ route($item->url) }}@else{{ $item->url }}@endif" id="{{ $item->name }}" @if($item->children->count() > 0) role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @endif>{{ $item->name }}</a>
     @endrole
     @else
-    <a class="nav-link @if($item->children->count() > 0) dropdown-toggle @endif" href="@if($item->is_internal == 1){{ route($item->url) }}@else{{ $item->url }}@endif" id="{{ $item->name }}" @if($item->children->count() > 0) role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @endif @if($item->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($item->name == 'Account Menu'){{ Auth::user()->name }}@elseif($item->name == 'Cart'){{$item->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $item->name }}@endif</a>
+    <a class="nav-link @if($item->children->count() > 0) dropdown-toggle @endif" href="@if($item->is_internal == 1){{ route($item->url) }}@else{{ $item->url }}@endif" id="{{ $item->name }}" @if($item->children->count() > 0) role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @endif @if($item->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($item->name == 'Account Menu'){{ Auth::user()->name }} <img src="{{ avatar_image_url(Auth::user(), 20) }}" class="rounded-circle"> @elseif($item->name == 'Cart'){{$item->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $item->name }}@endif</a>
     @endif
     @if($item->name == 'Logout')
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -21,7 +21,7 @@
         <li><a class="dropdown-item" href="@if($childItem->is_internal == 1){{ route($childItem->url) }}@else{{ $childItem->url }}@endif">{{ $childItem->name }}</a></li>
         @endrole
         @else
-        <li><a class="dropdown-item" href="@if($childItem->is_internal == 1){{ route($childItem->url) }}@else{{ $childItem->url }}@endif" @if($childItem->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($childItem->name == 'Account Menu'){{ Auth::user()->name }}@elseif($childItem->name == 'Cart'){{$childItem->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $childItem->name }}@endif</a></li>
+        <li><a class="dropdown-item" href="@if($childItem->is_internal == 1){{ route($childItem->url) }}@else{{ $childItem->url }}@endif" @if($childItem->name == 'Logout') onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @else @endif>@if($childItem->name == 'Account Menu'){{ Auth::user()->name }} <img src="{{ avatar_image_url(Auth::user(), 20) }}" class="rounded-circle"> @elseif($childItem->name == 'Cart'){{$childItem->name }}@if(Cart::isNotEmpty())<span class="badge badge-pill badge-secondary">{{ Cart::itemCount() }}</span> @endif @else{{ $childItem->name }}@endif</a></li>
         @endif
         @if($childItem->name == 'Logout')
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
