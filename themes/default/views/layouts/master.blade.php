6b501ce4f35dd0e84e1cb012fd1fcbe0300e53ef
Joshua Webb <joshwebb84@outlook.com>
2023-04-25 22:42:44 -0600

    Updated Bootstrap to 5.2.3 (was mistaken on latest version) and the default theme CSS is built off customized Bootstrap SASS

--- themes/default/views/layouts/master.blade.php
+++ themes/default/views/layouts/master.blade.php
@@ -15,12 +15,11 @@
 
         <!-- Styles -->
         <link href="{{ themes('css/bootstrap.css') }}" rel="stylesheet">
-        <link href="{{ themes('css/app.css') }}" rel="stylesheet">
         <link href="{{ themes('css/custom.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-expand-lg navbar-dark navbar-simplicms">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'SimpliCMS') }}</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
