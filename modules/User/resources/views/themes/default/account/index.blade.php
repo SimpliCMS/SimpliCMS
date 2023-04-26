e637618e390d88849715c3e03ca8fd8c3733623c
Joshua Webb <joshwebb84@outlook.com>
2023-04-25 19:44:19 -0600

    adding base structure for root plugins

--- modules/User/resources/views/themes/default/account/index.blade.php
+++ modules/User/resources/views/themes/default/account/index.blade.php
@@ -9,44 +9,48 @@
                 <div class="card-header">{{ __('Account Details') }}</div>
 
                 <div class="card-body">
-                    <form action="{{ route('user.update', ['id'=> $user->id]) }}" method="POST" class="needs-validation" novalidate>
+                    <form action="{{ route('user.update', ['id'=> $user->id]) }}" method="POST" class="needs-validation justify-content-center" novalidate>
                         @csrf
-                        @method('PUT')
-                        <div class="form-floating mb-3 row">
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ $user->name }}" placeholder="Name" required autofocus>
-                            <label for="name">{{ __('Name') }}</label>
-                            @if ($errors->has('name'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('name') }}
-                            </div>
-                            @endif
-                        </div>
+                        @method('PUT') 
+                        <div class="row justify-content-center">
+                            <div class="col-sm-10">
+                                <div class="form-floating mb-3 row">
+                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ $user->name }}" placeholder="Name" required autofocus>
+                                    <label for="name">{{ __('Name') }}</label>
+                                    @if ($errors->has('name'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('name') }}
+                                    </div>
+                                    @endif
+                                </div>
 
-                        <div class="form-floating mb-3 row">
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $user->email }}" placeholder="name@example.com" required>
-                            <label for="email">{{ __('E-Mail Address') }}</label>
-                            @if ($errors->has('email'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('email') }}
-                            </div>
-                            @endif
-                        </div>
+                                <div class="form-floating mb-3 row">
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $user->email }}" placeholder="name@example.com" required>
+                                    <label for="email">{{ __('E-Mail Address') }}</label>
+                                    @if ($errors->has('email'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('email') }}
+                                    </div>
+                                    @endif
+                                </div>
+                                <div class="d-grid gap-2 mx-auto">
+                                    <button type="submit" class="btn btn-primary btn-lg">
+                                        {{ __('Update') }}
+                                    </button>
 
-                        <div class="input-group row mb-0">
-                            <button type="submit" class="btn btn-primary btn-lg">
-                                {{ __('Update') }}
-                            </button>
+                                </div>
+                            </div>
+
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
-<script>
-    window.onload = function () {
-        var element = document.getElementById("account");
-        element.classList.add("active");
-    };
-</script>
+    @endsection
+    <script>
+        window.onload = function () {
+            var element = document.getElementById("account");
+            element.classList.add("active");
+        };
+    </script>
