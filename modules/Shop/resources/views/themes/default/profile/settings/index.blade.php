48cc936705234e9c600354abf098a9afc0ce5642
TohurTV <joshwebb84@outlook.com>
2023-05-04 15:18:58 -0600

    Placeholder form fields for the shop profile settings

--- modules/Shop/resources/views/themes/default/profile/settings/index.blade.php
+++ modules/Shop/resources/views/themes/default/profile/settings/index.blade.php
@@ -11,10 +11,59 @@
                 <div class="p-4 p-md-5">
                     <h3 class="text-center mt-4 mb-4">{{ __('Shop Profile') }}</h3>
                     <div class="card-body">
-                        <form method="POST" action="{{ route('profile.settings.shop.update', ['user' => $user, 'profile' => $profile]) }}" enctype="multipart/form-data">
+                        <form class="row g-3" method="POST" action="{{ route('profile.settings.shop.update', ['user' => $user, 'profile' => $profile]) }}" enctype="multipart/form-data">
                             @csrf
                             @method('PUT')
- 
+                            <h5>{{ __('Billing Address') }}</h5>
+                            <div class="col-12">
+                                <label for="inputAddress" class="form-label">Address</label>
+                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                            </div>
+                            <div class="col-12">
+                                <label for="inputAddress2" class="form-label">Address 2</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="inputCity" class="form-label">City</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="inputState" class="form-label">State</label>
+                                <select id="inputState" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <label for="inputZip" class="form-label">Zip</label>
+                                <input type="text" class="form-control" id="inputZip">
+                            </div>
+                            
+                            <h5>{{ __('Shipping Address') }}</h5>
+                            <div class="col-12">
+                                <label for="inputAddress" class="form-label">Address</label>
+                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                            </div>
+                            <div class="col-12">
+                                <label for="inputAddress2" class="form-label">Address 2</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="inputCity" class="form-label">City</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="inputState" class="form-label">State</label>
+                                <select id="inputState" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <label for="inputZip" class="form-label">Zip</label>
+                                <input type="text" class="form-control" id="inputZip">
+                            </div>
+                            
                             <div class="d-grid gap-2 mx-auto">
                                 <button type="submit" class="btn btn-primary">{{ __('Update') }}</button>
                             </div>
