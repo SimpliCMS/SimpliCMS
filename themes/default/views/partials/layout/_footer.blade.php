19523fc94e1d0e5a87268758e20daab44f0c9c49
TohurTV <joshwebb84@outlook.com>
2023-05-05 21:29:39 -0600

    adding footer menu

--- themes/default/views/partials/layout/_footer.blade.php
+++ themes/default/views/partials/layout/_footer.blade.php
@@ -1,5 +1,8 @@
 <div class="container">
-    <footer class="pt-3 mt-4 text-muted border-top">
-        &copy; {{ now()->year }} {{ config('app.name', 'SimpliCMS') }}
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">&copy; {{ now()->year }} {{ config('app.name', 'SimpliCMS') }}</p>
+        <ul class="nav col-md-4 justify-content-end">
+            @include('partials.menu.main', ['menuItems' => Core::getMenu('Footer Menu')])
+        </ul>
     </footer>
 </div>
